053b28e31391f258ccb544994a60ddab9b42bd49
TheSaviourEking <saviour.eking@outlook.com>
2024-06-19 16:40:54 +0100

    chore: fix the db port
    the db port was conflicting with the server port when threw an error that prevented the app from running

--- config/index.js
+++ config/index.js
@@ -3,8 +3,9 @@ module.exports = {
   port: process.env.PORT || 5000,
   db: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   }
 };
